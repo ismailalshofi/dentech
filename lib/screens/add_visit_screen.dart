ea952ca7b9940f20695f522ea9566d7664ecc34f
ismailalshofi <ialshofi@gmail.com>
2023-11-07 23:26:07 +0300

    add visit screen

--- lib/screens/add_visit_screen.dart
+++ lib/screens/add_visit_screen.dart
@@ -30,6 +30,12 @@ class _AddVisitScreenState extends State<AddVisitScreen> {
   String selectedTreatment = "";
   DateTime selecteDate = DateTime.now();
   final selectedPatient = TextEditingController();
+  final description = TextEditingController();
+
+  // jaw and teeth
+
+  int? selectedTeeth;
+  int? selectedJaw;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,6 +59,12 @@ class _AddVisitScreenState extends State<AddVisitScreen> {
             _dateAndLocation(),
             15.height,
             _treatmentType(),
+            15.height,
+            _treatedTeeth(),
+            15.height,
+            _description(),
+            15.height,
+            _images(),
           ],
         ).paddingAll(10.sp),
       ),
@@ -69,6 +81,8 @@ class _AddVisitScreenState extends State<AddVisitScreen> {
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(15.sp),
             color: AppColors.silver,
+            border:
+                Border.all(color: AppColors.black.withOpacity(0.5), width: 1),
           ),
           child: TypeAheadField(
             textFieldConfiguration: TextFieldConfiguration(
@@ -147,6 +161,8 @@ class _AddVisitScreenState extends State<AddVisitScreen> {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(15.sp),
                   color: AppColors.silver,
+                  border: Border.all(
+                      color: AppColors.black.withOpacity(0.5), width: 0.8),
                 ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -189,8 +205,7 @@ class _AddVisitScreenState extends State<AddVisitScreen> {
                       ),
                     ),
                   ],
-                )
-                ),
+                )),
           ],
         ),
         Column(
@@ -211,6 +226,8 @@ class _AddVisitScreenState extends State<AddVisitScreen> {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(15.sp),
                   color: AppColors.silver,
+                  border: Border.all(
+                      color: AppColors.black.withOpacity(0.5), width: 0.8),
                 ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -246,6 +263,8 @@ class _AddVisitScreenState extends State<AddVisitScreen> {
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(15.sp),
               color: AppColors.silver,
+              border: Border.all(
+                  color: AppColors.black.withOpacity(0.5), width: 0.8),
             ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -264,7 +283,8 @@ class _AddVisitScreenState extends State<AddVisitScreen> {
                         fontSize: 12.sp,
                       ),
                     ),
-                    value: selectedTreatment.isNotEmpty ? selectedTreatment : null,
+                    value:
+                        selectedTreatment.isNotEmpty ? selectedTreatment : null,
                     icon: const Icon(Icons.keyboard_arrow_down),
                     style: TextStyle(
                       color: AppColors.black,
@@ -277,7 +297,8 @@ class _AddVisitScreenState extends State<AddVisitScreen> {
                         selectedTreatment = value!;
                       });
                     },
-                    items: treatments.map<DropdownMenuItem<String>>((String value) {
+                    items: treatments
+                        .map<DropdownMenuItem<String>>((String value) {
                       return DropdownMenuItem<String>(
                         value: value,
                         child: Text(value),
@@ -287,6 +308,245 @@ class _AddVisitScreenState extends State<AddVisitScreen> {
                 ),
               ],
             )),
+      ],
+    );
+  }
+
+  Widget _treatedTeeth() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Treated Teeth",
+          style: TextStyle(
+            fontSize: 12.sp,
+            color: AppColors.darkBlue,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        10.height,
+        Container(
+          width: 330.w,
+          // height: 45.h,
+          margin: const EdgeInsets.symmetric(vertical: 5),
+          decoration: BoxDecoration(
+            color: AppColors.silver,
+            borderRadius: BorderRadius.circular(20.sp),
+            border:
+                Border.all(color: AppColors.black.withOpacity(0.5), width: 1),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _jawWidget(),
+              VerticalDivider(
+                      thickness: 1, color: AppColors.black.withOpacity(0.6))
+                  .withHeight(120.h),
+              _teethNumbers(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _jawWidget() {
+    return Column(
+      children: [
+        25.height,
+        Row(
+          children: [
+            Transform.flip(
+              flipX: true,
+              child: Image.asset(
+                'assets/icons/jaw.png',
+                color: selectedJaw == 0
+                    ? AppColors.blue
+                    : AppColors.black.withOpacity(0.6),
+                height: 60.h,
+              ),
+            ).onTap(() => setState(() => selectedJaw = 0)),
+            20.width,
+            Transform.flip(
+              flipX: false,
+              child: Image.asset(
+                'assets/icons/jaw.png',
+                color: selectedJaw == 1
+                    ? AppColors.blue
+                    : AppColors.black.withOpacity(0.6),
+                height: 60.h,
+              ),
+            ).onTap(() => setState(() => selectedJaw = 1)),
+          ],
+        ),
+        Divider(thickness: 1.5, color: AppColors.black.withOpacity(0.6))
+            .withWidth(80.w),
+        Row(
+          children: [
+            Transform.flip(
+              flipX: true,
+              flipY: true,
+              child: Image.asset(
+                'assets/icons/jaw.png',
+                color: selectedJaw == 2
+                    ? AppColors.blue
+                    : AppColors.black.withOpacity(0.6),
+                height: 60.h,
+              ),
+            ).onTap(() => setState(() => selectedJaw = 2)),
+            20.width,
+            Transform.flip(
+              flipX: false,
+              flipY: true,
+              child: Image.asset(
+                'assets/icons/jaw.png',
+                color: selectedJaw == 3
+                    ? AppColors.blue
+                    : AppColors.black.withOpacity(0.6),
+                height: 60.h,
+              ),
+            ).onTap(() => setState(() => selectedJaw = 3)),
+          ],
+        ),
+        25.height,
+      ],
+    );
+  }
+
+  Widget _teethNumbers() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: List.generate(
+            3,
+            (index) => Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              decoration: BoxDecoration(
+                color:
+                    selectedTeeth == index ? AppColors.blue : AppColors.silver,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: AppColors.black.withOpacity(0.5),
+                  width: 1,
+                ),
+              ),
+              child: Text(
+                (index + 1).toString(),
+                style: TextStyle(
+                  fontSize: 12.sp,
+                  color: selectedTeeth == index
+                      ? AppColors.white
+                      : AppColors.black.withOpacity(0.5),
+                  fontWeight: FontWeight.w500,
+                ),
+              ).paddingSymmetric(vertical: 10, horizontal: 20),
+            ).onTap(() => setState(() => selectedTeeth = index)),
+          ),
+        ),
+        20.height,
+        Row(
+          children: List.generate(
+            3,
+            (index) => Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              decoration: BoxDecoration(
+                color: selectedTeeth == index + 3
+                    ? AppColors.blue
+                    : AppColors.silver,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: AppColors.black.withOpacity(0.5),
+                  width: 1,
+                ),
+              ),
+              child: Text(
+                (index + 4).toString(),
+                style: TextStyle(
+                  fontSize: 12.sp,
+                  color: selectedTeeth == index + 3
+                      ? AppColors.white
+                      : AppColors.black.withOpacity(0.6),
+                  fontWeight: FontWeight.w500,
+                ),
+              ).paddingSymmetric(vertical: 10, horizontal: 20),
+            ).onTap(() => setState(() => selectedTeeth = index + 3)),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _description() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Description",
+          style: TextStyle(
+            fontSize: 12.sp,
+            color: AppColors.darkBlue,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        10.height,
+        Container(
+          // height: 150.h,
+          width: 320.w,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: AppColors.silver,
+             border: Border.all(color: AppColors.black.withOpacity(0.5), width: 1),
+          ),
+          child: TextFormField(
+            maxLines: 8,
+            decoration: InputDecoration(
+              enabledBorder: InputBorder.none,
+              focusedBorder: InputBorder.none,
+              hintText: 'type something or hold the mice',
+              suffixIcon: IconButton(onPressed: (){} , icon: Icon(Icons.mic_external_on),),
+              contentPadding: const EdgeInsets.all(15),
+              hintStyle: TextStyle(
+                color: AppColors.black.withOpacity(0.37),
+                fontWeight: FontWeight.w500,
+                fontSize: 14.sp,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _images(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Attachments",
+          style: TextStyle(
+            fontSize: 12.sp,
+            color: AppColors.darkBlue,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        10.height,
+        Container(
+            height: 40.h,
+              width: 120.w,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.sp),
+                color: AppColors.silver,
+                border:Border.all(color: AppColors.black.withOpacity(0.5), width: 1),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text("Upload", style: TextStyle(color: AppColors.black.withOpacity(0.6)),),
+                  Icon(Icons.upload, color: AppColors.black.withOpacity(0.6)),
+                ],
+              ),
+        ),
       ],
     );
   }
